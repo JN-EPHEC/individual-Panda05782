8ed5b2158e1d3e806c0bca150ee6491f89c3bcc4
Panda05782 <panda.seconde.782@gmail.com>
2026-02-10 21:40:35 +0100

    [ADD] tp01 exercice 6.1 API et TABLEAU

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,23 @@ const port: number = 3000; // Typage explicite du port
 
 // Route GET sur la racine /
 // Défi typage relevé : on précise que 'req' est de type Request et 'res' de type Response
+interface Etudiant {
+    id: number;
+    nom: string;
+    prenom: string;
+}
+// Ton tableau de données typé
+const etudiants: Etudiant[] = [
+    { id: 1, nom: "Dupont", prenom: "Jean" },
+    { id: 2, nom: "Martin", prenom: "Sophie" },
+    { id: 3, nom: "Doe", prenom: "John" },
+];
+
+// La route GET /api/data
+app.get('/api/data', (req: Request, res: Response) => {
+    res.json(etudiants);
+});
+
 app.get('/', (req: Request, res: Response) => {
     res.send("Bienvenue sur mon serveur API");
 });
