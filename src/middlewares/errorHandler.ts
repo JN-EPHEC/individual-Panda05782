c5567a713f403089c2e8baa3194070dc60fdd2a3
Panda05782 <panda.seconde.782@gmail.com>
2026-03-02 21:45:21 +0100

    [ADD] simplification ex 3.3 modif ds pack.json

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,19 @@
+import type { Request, Response, NextFunction } from 'express';
+
+export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+    console.error(err);
+
+    const status = err.status || err.statusCode || 500;
+    const message = err.message || 'Une erreur est survenue';
+
+    res.status(status).json({ error: message });
+};
+
+export const checkIdParam = (err: any, req: Request, res: Response, next: NextFunction) => {
+    console.error(err);
+
+    const statusID = err.status || err.statusCode || 400;
+    const message = err.message || 'Une erreur est survenue';
+
+    res.status(statusID).json({ error: message });
+};
